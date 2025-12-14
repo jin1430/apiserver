9396e2ab60b9eb6f89bb5b82531be38a2d87f43c
LastYou <jj1430413@gmail.com>
2025-12-15 07:48:26 +0900

    DB 방식 RDS로 변경7

--- api_server/src/main/java/com/busproject/apiserver/entity/Stop.java
+++ api_server/src/main/java/com/busproject/apiserver/entity/Stop.java
@@ -6,7 +6,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "stops")
+@Table(name = "STOPS")
 public class Stop {
     @Id
     @Column(name = "stop_id")
